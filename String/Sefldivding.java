264cf34163adafc05febb40ee04e284a1fe46e3a
Shutian Ma <mashutian0608@hotmail.com>
2018-08-14 17:34:43 -0400

    Update and rename sefldivding.java to Sefldivding.java

--- String/Sefldivding.java
+++ String/Sefldivding.java
@@ -1,3 +1,10 @@
+/**
+*A self-dividing number is a number that is divisible by every digit it contains.
+*For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+*Also, a self-dividing number is not allowed to contain the digit zero.
+*Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+*/
+
 //needs to be improved
 
 class Solution {
@@ -7,7 +14,7 @@ class Solution {
         for(int i =0;i< charlist.length;i++)
         {
             if(charlist[i]=='0') return false;
-            if(input%(Integer.parseInt(Character.toString(charlist[i])))!=0) return false;
+            if(input%(Character.getNumericValue(charlist[i]))>0) return false;
         }
         return true;
     }
